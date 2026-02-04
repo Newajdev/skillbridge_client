f97e9c09212124f6ea04ac1b40cca3f5eaa55117
newajDev <newaj.design@gmail.com>
2026-02-04 20:33:11 +0600

    resolve the error to deploy the project on the vercel

---
+++ src/env.ts
@@ -0,0 +1,4 @@
+export const env = {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+    NEXT_PUBLIC_AUTH_URL: process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:5000/api/auth",
+};
